0d09f8352832f9e6c81c4dbcdfb79c98d61b9e35
Victor Nunez <vnunez0914@gmail.com>
2019-09-13 20:21:32 -0500

    added jpeg images, made triviaQuestions object

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+
+//object and images 
+
+var triviaQuestions = [
+    {
+        question: "In what year did Spider-Man make his comic debut",
+        choices: ["1962", "2000", "1986", "1990"],
+        answer: "1962"
+    },
+    {
+        question: "What is Magnetos special Power",
+        choices: ["Magnetism, Telepathy, Flight, Super Strength"],
+        answer: "Magnetism"
+    },
+
+    {
+        question: "What is the name of Thor's hammer",
+        choices: ["Stormbreaker, Jarnbjorn, Thunder Strike, Mjolnir"],
+        answer: "Mjolnir"
+    },
+
+    {
+        questions: "Which one of these Heroes does not belong in the Fantastic Four?",
+        choices: ["Thing", "Human Torch", "Mister Fantastic", "Mystique"],
+        answer: "Mystique"
+    },
+
+    {
+        questions: "What is Steve Roger's super hero name?",
+        choices: ["Wolverine", "Hawkeye", "Captain America", "Cable"],
+        answer: "Captain America"
+    }
+];
+    console.log(triviaQuestions)
